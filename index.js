abb9c644451dde5313f4175d02c053911688d5fd
Rachel Hamby <tnthreat33@gmail.com>
2023-09-28 18:29:39 -0400

    commit tree 0eaaa18f2896669033113ec15b2edee4ab0b75f7

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = "Rachel"
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = welcomeMessage.toUpperCase ()
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+
